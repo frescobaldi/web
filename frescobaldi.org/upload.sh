ed1f5bbbaa990b439393fca134bbe8ce6f67f3b9
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-01-21 10:16:42 +0100

    update source urls

--- frescobaldi.org/upload.sh
+++ frescobaldi.org/upload.sh
@@ -1,5 +1,5 @@
 rsync -ave ssh \
   --exclude '*~' \
   --exclude '/upload.sh' \
-  . wilbertb@wilbertberendsen.nl:/srv/wilbertb/www/www.frescobaldi.org/
+  . wilbertb@wilbertberendsen.nl:/home/wilbertb/domains/frescobaldi.org/public_html/
 
